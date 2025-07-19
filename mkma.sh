81761d74bf7a8eef505e330ae90d89fb57c0fc50
I <root@raja>
2025-07-20 01:35:46 +0300

    Initramfs arguments

--- mkma.sh
+++ mkma.sh
@@ -58,7 +58,7 @@ passwd -d root
 passwd -d i
 su -c '
     git clone https://github.com/israellevin/dotfiles.git ~/src/dotfiles
-    ~/src/dotfiles/install.sh' i
+    ~/src/dotfiles/install.sh --non-interactive' i
 EOF
     reset
 }
@@ -68,7 +68,7 @@ mkcpio() {
     find . -mount -print0 | pv -0 -l -s "$(find . | wc -l)" | cpio -o --null --format=newc | zstd -T0 -$level
 }
 
-mkinitramfsinit() {
+mkinit() {
     cat <<'EOF' > ./init
 #!/bin/sh
 new_root_path=/root
@@ -77,64 +77,80 @@ mount -t devtmpfs devtmpfs /dev
 _log() {
     level=$1
     shift
-    message="$(date) [mkma.sh init]: $*"
+    message="$(date) [mkma.sh init]: $@"
     echo "<$level>$message" > /dev/kmsg
 }
-info() { _log 6 "Info: $*"; }
-error() { _log 3 "Error: $*"; }
-emergency() { _log 2 "Emergency: $*"; /bin/sh; }
+info() { _log 6 "Info: $@"; }
+error() { _log 3 "Error: $@"; }
+emergency() { _log 2 "Emergency: $@"; /bin/sh; }
 
 info Starting init process
 
-info Mounting tmpfs for new root
+info Mounting tmpfs for new root on "$new_root_path"
 mkdir -p "$new_root_path"
 mount -t tmpfs -o size=8G tmpfs "$new_root_path"
 
-info Getting mkma storage
+info Getting mkma storage parameters
 mkdir -p /proc /mnt
 mount -t proc proc /proc
+mount -t sysfs sysfs /sys
 for arg in $(cat /proc/cmdline); do
     case "$arg" in
         mkma_storage_device=*)
             mkma_storage_device="${arg#mkma_storage_device=}"
             ;;
         mkma_storage_path=*)
-            mkma_storage_path="${arg#mkma_storage_path=}"
+            mkma_storage_path="${arg#mkma_storage_path=/}"
             ;;
     esac
 done
+
+info Mounting mkma storage device "'$mkma_storage_device'"
+modprobe nvme
 modprobe crc32c_generic
 modprobe ext4
-mount "$mkma_storage_device" /mnt || emergency Could not mount mkma storage device "'$mkma_storage_device'"
-[ -f "/mnt/$mkma_storage_path" ] || emergency Could not find root image at "'/mnt/$mkma_storage_path'"
+modprobe virtio_blk || true  # Just for qemu testing.
+modprobe virtio_pci || true  # Just for qemu testing.
+mount "$mkma_storage_device" /mnt || \
+    emergency Could not mount mkma storage device "'$mkma_storage_device'"
+mkma_storage_path="/mnt/$mkma_storage_path"
 
-info Copying root image data
+info Checking mkma storage path "'$mkma_storage_path'"
+[ -f "$mkma_storage_path" ] || \
+    emergency Could not find root image at "'$mkma_storage_path'"
+
+info Copying root image data from "'$mkma_storage_path'" to "'$new_root_path'"
 cd "$new_root_path"
-pv -pterab /mnt"$mkma_storage_path" | zstd -dcf | cpio -id || emergency Could not copy root image data
+pv -pterab "$mkma_storage_path" | zstd -dcf | cpio -id || \
+    emergency "Could not copy root image data from $mkma_storage_path'"
 umount /mnt
 
-info Moving to new root
-exec run-init /root /lib/systemd/systemd || emergency Failed pivot to systemd
+info Moving to new root on "$new_root_path"
+exec run-init /root /lib/systemd/systemd || \
+    emergency Failed pivot to systemd on "$new_root_path"
 EOF
 
     chmod +x ./init
 }
 
 mkinitramfs() {
+    local modules="$1"
+    local binaries="$2"
     mkdir -p ./{bin,dev,mnt,proc,sys,run,tmp}
     chmod 1777 ./tmp
     mknod -m 622 ./dev/console c 5 1 2>/dev/null || true
     mknod -m 666 ./dev/null c 1 3 2>/dev/null || true
 
     cp -a --parents /lib/modules/"$(uname -r)"/modules.dep .
-    for required_module in ext4 pci; do
+    for required_module in $modules; do
+        echo $module
         for dependency_module in $(modprobe --show-depends $required_module | cut -d' ' -f2); do
             mkdir -p ".$(dirname "$dependency_module")"
             cp -au --parents "$dependency_module" .
         done
     done
 
-    for binary in busybox pv zstd; do
+    for binary in $binaries; do
         binary="$(type -p $binary)"
         cp -a "$binary" ./bin/.
         for library in $(ldd "$binary" 2> /dev/null | grep -o '/[^ ]*'); do
@@ -148,10 +164,10 @@ mkinitramfs() {
     done
     cd -
 
-    mkinitramfsinit
+    mkinit
 }
 
-bootinitramfs() {
+test_on_qemu() {
     local kernel_image="$1"
     local initramfs_image="$2"
     local root_image="$3"
@@ -159,12 +175,10 @@ bootinitramfs() {
     local ramdisk_size="$5"
 
     if [ ! -f "$qemu_disk" ]; then
-        set -x
         qemu-img create -f raw "$qemu_disk" $ramdisk_size
-        set +x
         mkfs.ext4 -F "$qemu_disk"
     fi
-    mkdir ./mnt
+    mkdir -p ./mnt
     mount "$qemu_disk" ./mnt
     cp --parents "$root_image" ./mnt/.
     umount ./mnt
@@ -174,12 +188,11 @@ bootinitramfs() {
         -m "$ramdisk_size"
         -kernel "$kernel_image"
         -initrd "$initramfs_image"
-        -append "console=tty root=/dev/ram0 init=/init mkma_storage_device=/dev/nvme0n1 mkma_storage_path=$root_image"
+        -append "console=tty root=/dev/ram0 init=/init mkma_storage_device=/dev/vda mkma_storage_path=$root_image"
         -netdev user,id=mynet0
         -device e1000,netdev=mynet0
+        -drive file="$qemu_disk",format=raw,if=virtio,cache=none
         -enable-kvm
-        -drive file="$qemu_disk",format=raw,if=none,id=nvme0
-        -device nvme,drive=nvme0,serial=deadbeef
     )
 
     if [ "$QEMU_VGA" ]; then
@@ -203,22 +216,27 @@ mkma() {
     local root_image="$PWD/mkma.root.cpio.zst"
     local initramfs_image="$PWD/mkma.init.cpio.zst"
     local qemu_disk="$PWD/mkma.qemu.disk.raw"
+    local initramfs_binaries=(busybox pv zstd lsblk)
+    local initramfs_modules=(ext4 nvme pci)
     local packages=(
         dbus dbus-user-session systemd-sysv udev
         coreutils klibc-utils kmod util-linux
         bash bash-completion chafa console-setup git git-delta locales mc tmux vim
         cpio gzip tar unrar unzip zstd
-        bc bsdextrautils bsdutils mawk moreutils pciutils psmisc sed ripgrep usbutils
+        bc bsdextrautils bsdutils mawk moreutils pciutils psmisc pv sed ripgrep usbutils
         ca-certificates dhcpcd5 iproute2 netbase
         aria2 curl iputils-ping openssh-server w3m wget
         firmware-iwlwifi iw wpasupplicant
         docker.io docker-cli nodejs npm python3-pip python3-venv
-        foot firefox wl-clipboard wmenu
         ffmpeg mpv pipewire-audio yt-dlp
+        firmware-intel-graphics foot firefox wl-clipboard wmenu
     )
 
-    if [ "$QEMU_VGA" ]; then
-        packages+=(mesa-utils libgl1-mesa-dri pciutils)
+    if ! [ "$DISABLE_QEMU_TESTING" ]; then
+        initramfs_modules+=(virtio_pci virtio_blk)
+        if [ "$QEMU_VGA" ]; then
+            packages+=(mesa-utils libgl1-mesa-dri pciutils)
+        fi
     fi
 
     mkcd "$chroot_dir"
@@ -228,12 +246,15 @@ mkma() {
     mkuser
     mkcpio "$COMPRESSION_LEVEL" > "$root_image"
 
+    rm -rf "$initramfs_dir"
     mkcd "$initramfs_dir"
-    [ -f ./init ] || mkinitramfs
+    mkinitramfs "${initramfs_modules[*]}" "${initramfs_binaries[*]}"
     mkcpio "$COMPRESSION_LEVEL" > "$initramfs_image"
 
-    echo Testing mkma on QEMU...
-    bootinitramfs /boot/vmlinuz-$(uname -r) "$initramfs_image" "$root_image" "$qemu_disk" 2G
+    if ! [ "$DISABLE_QEMU_TESTING" ]; then
+        echo Testing mkma on QEMU...
+        test_on_qemu /boot/vmlinuz-$(uname -r) "$initramfs_image" "$root_image" "$qemu_disk" 4G
+    fi
 
     echo kernel: /boot/vmlinuz-$(uname -r)
     echo initramfs: "$initramfs_image"
