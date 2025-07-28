aa09e010da24b529e2bd716ace601834ac6b8406
I <root@raja>
2025-07-28 23:43:41 +0300

    Add wlrctl

--- mkma.sh
+++ mkma.sh
@@ -281,7 +281,7 @@ mkma() {
         firmware-iwlwifi iwd
         debootstrap docker.io docker-cli nodejs npm python3-pip python3-venv
         bluez ffmpeg mpv pipewire-audio yt-dlp
-        cliphist firmware-intel-graphics foot firefox wl-clipboard wmenu xwayland
+        cliphist firmware-intel-graphics foot firefox wl-clipboard wlrctl wmenu xwayland
         libxcb-composite0 libxcb-errors0 libxcb-ewmh2 libxcb-icccm4 libxcb-render-util0
         libxcb-render0 libxcb-res0 libxcb-xinput0 libgles2 libinput10 libliftoff0 libseat1
     )
